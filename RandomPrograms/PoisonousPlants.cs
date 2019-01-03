33a0ce49a2d2c8773e73f5eee41a4fdd5ab658a5
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2019-01-03 11:52:52 +0800

    Poisnous plant using list

--- RandomPrograms/PoisonousPlants.cs
+++ RandomPrograms/PoisonousPlants.cs
@@ -9,7 +9,7 @@ namespace temp
     class PoisonousPlants
     {
         //Dictionary way of doing
-        public static int poisonousPlants(int[] p)
+        public static int poisonousPlants1(int[] p)
         {
             Dictionary<int, int> dict = new Dictionary<int, int>();
             int n = p.Length;
@@ -54,6 +54,109 @@ namespace temp
             }
 
             return loop - 1;
+        }
+
+        public static int poisonousPlants2(int[] p)
+        {
+            Stack<int> st = new Stack<int>();
+            Stack<int> st1 = new Stack<int>();
+            int n = p.Length;
+            int loop = 0;
+            bool found = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                st.Push(i);
+            }
+
+            while (found)
+            {
+                found = false;
+                st1 = new Stack<int>();
+
+                var pop = st.Pop();
+                while (st.Count != 0)
+                {
+                    if (p[pop] <= p[st.Peek()])
+                    {
+                        st1.Push(pop);
+                    }
+                    else
+                    {
+                        found = true;
+                    }
+
+                    pop = st.Pop();
+                }
+
+                st1.Push(pop);
+
+                while (st1.Count != 0)
+                {
+                    var pop1 = st1.Pop();
+                    st.Push(pop1);
+                }
+
+                if (found)
+                    loop++;
+            }
+
+            return loop;
+        }
+
+        public static int poisonousPlants(int[] p)
+        {
+            List<List<int>> stackList = new List<List<int>>();
+            List<int> st = new List<int>();
+            st.Add(p[0]);
+            for (int i = 1; i < p.Length; i++)
+            {
+                if (p[i] <= p[i - 1])
+                {
+                    st.Add(p[i]);
+                }
+                else
+                {
+                    stackList.Add(st);
+
+                    st = new List<int>();
+                    st.Add(p[i]);
+                }
+
+                if (i == p.Length - 1)
+                    stackList.Add(st);
+            }
+
+            int dayCount = 0;
+
+            while (stackList.Count != 1)
+            {
+                for (int i = stackList.Count - 1; i > 0; i--)
+                {
+                    if (stackList[i].Count > 0)
+                    {
+                        stackList[i].RemoveAt(0);
+                    }
+
+                    if (stackList[i].Count == 0)
+                    {
+                        stackList.Remove(stackList[i]);
+                    }
+                }
+
+                for (int i = stackList.Count - 1; i > 0; i--)
+                {
+                    if (stackList[i][0] <= stackList[i - 1][stackList[i - 1].Count - 1])
+                    {
+                        stackList[i - 1].AddRange(stackList[i]);
+                        stackList.Remove(stackList[i]);
+                    }
+                }
+
+                dayCount++;
+            }
+
+            return dayCount;
         }
     }
 }
